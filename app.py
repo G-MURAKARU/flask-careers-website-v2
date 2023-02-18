378c184ace9b7434895f6d64a4154bea4e9f56ce
Gicheru Murakaru <85207521+G-MURAKARU@users.noreply.github.com>
2023-02-18 16:11:27 +0000

    commit applications to database

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db, load_job_from_db
+from flask import Flask, render_template, jsonify, request
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 
 app = Flask(__name__)
 
@@ -19,6 +19,15 @@ def list_jobs():
 def show_job(id):
 	job = load_job_from_db(id)
 	return render_template('job_page.html', job=job, title=job['title'])
+
+
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+	# request is used to extract url params
+	data = request.form
+	job = load_job_from_db(id)
+	add_application_to_db(id, data)
+	return render_template('application_submitted.html', application=data, job=job, title='Application Submitted')
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -23,3 +23,9 @@ def load_job_from_db(id):
 		if results:
 			return {key:val for key, val in zip(KEYS, results[0])}
 		return None
+
+
+def add_application_to_db(id, data):
+	with engine.connect() as conn:
+		query = text(f'INSERT INTO applications(job_id, full_name, email, linkedin, education, work, resume_url) VALUES ({id}, "{data["full_name"]}", "{data["email"]}", "{data["linkedin"]}", "{data["education"]}", "{data["work"]}", "{data["resume_url"]}")')
+		conn.execute(query)
